9276b84b108cd860d1ed4dce33bdcee2f43627ab
xiong7569439 <xiong7569439@github.com>
2026-03-04 01:41:19 +0800

    Add wrangler.toml config for Cloudflare

--- next.config.js
+++ next.config.js
@@ -4,6 +4,12 @@ const nextConfig = {
   images: {
     unoptimized: true,
   },
+  // 明确设置服务器less模式（Cloudflare Workers兼容）
+  experimental: {
+    serverActions: {
+      bodySizeLimit: '2mb',
+    },
+  },
 };
 
 module.exports = nextConfig;
